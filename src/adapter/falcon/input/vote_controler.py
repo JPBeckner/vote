0736af9636934bb6685da34822bdead7272df517
JPBeckner <pedrobeckner@hotmail.com>
2022-04-02 03:00:04 -0300

    feat: upd controller

--- src/adapter/falcon/input/vote_controler.py
+++ src/adapter/falcon/input/vote_controler.py
@@ -14,21 +14,56 @@ class VoteController(Controller):
         self.service: RegistryVote = service
         self.logger = getLogger('app')
     
-    def on_get(self, req, resp, code=None):
+    def on_get_vote(self, req, resp, code=None):
         self.logger.debug("Handling a get request")
         vote = self.service.get_vote(code=code)
-        resp.text = dumps({
-            "code": vote.code,
-            "count": vote.count,
-        })
+        
         resp.set_header('Powered-By', 'Falcon')
         
-        resp.status = falcon.HTTP_200
+        if vote:
+            resp.text = vote.json()
+            resp.status = falcon.HTTP_200
+            return
+        
+        resp.status = falcon.HTTP_404
+        resp.text = dumps({
+            "message": f"Vote option with code {code} not found.",
+            "status": falcon.HTTP_404
+        })
     
     def on_post(self, req, resp):
         body: dict = load(req.stream)
-        vote = Vote(**body)
-        self.service.save_vote(vote=vote)
+        a = req.get_param_as_int('code')
+        b = req.context.get('code')
+        code = body.get('code')
+        
+        if not code:
+            raise
+        
+        self.service.save_vote(code=code)
+        
+        resp.status = falcon.HTTP_200
+        resp.text = dumps({
+            "message": f"Vote {code} saved successfully!",
+            "status": falcon.HTTP_200
+        })
+        
+    def on_put(self, req, resp):
+        body: dict = load(req.stream)
+        code = body.get('code')
+        
+        if not code:
+            raise
+        
+        self.service.create_vote_option(code=code)
+        
+        resp.status = falcon.HTTP_200
+        resp.text = dumps({
+            "message": f"Vote option {code} created successfully!",
+            "status": falcon.HTTP_200
+        })
+
+
     
     def on_delete(self, req, resp):
         pass
